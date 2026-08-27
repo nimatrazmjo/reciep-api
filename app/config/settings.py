9ef4cee97708432ef816201352a5413ea982760c
Nimat Razmjo <nimatullah.razmjo@gmail.com>
2026-08-27 14:17:27 -0400

    feat(core): add custom User model with email as username
    replace default django user with a custom one using email
    instead of username for auth, per AUTH_USER_MODEL best practice
    of setting this before the first migration

--- app/config/settings.py
+++ app/config/settings.py
@@ -130,3 +130,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+AUTH_USER_MODEL = 'core.User'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,43 @@
+""" Test for models """
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test Models"""
+
+    def test_create_user_with_email_successfull(self):
+        """Test creating a user with an email is sucessfull"""
+
+        email = 'test@example.com'
+        password = 'testpasss123'
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_normalize_new_user(self):
+        """ Test email is normalized for new users"""
+
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a value error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test1234')
+
+    def test_create_superuser(self):
+        """Test creating a super user."""
+
+        user = get_user_model().objects.create_superuser('test@example.com', 'test123')
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
